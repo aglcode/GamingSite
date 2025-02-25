2623963218783304a66199d942c91ab5b1b5d7f2
webc22696@gmail.com <webc26696@gmail.com>
2025-02-25 19:36:14 +0800

    Finished Gaming Site

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import Hero from './components/hero.jsx'
 import About from './components/about.jsx'
+import Features from './components/features.jsx'
+import Navbar from './components/navbar.jsx'
+import Story from './components/story.jsx'
+import Contact from './components/Contact.jsx'
+import Footer from './components/footer.jsx'
+import Button from './components/button.jsx'
 
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Navbar />
       <Hero />
       <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   )
 }
